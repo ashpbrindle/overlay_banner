d489a06633b9aa7e085474b6d2ef293229f24f45
Ash Brindle <ashb@nextbase.co.uk>
2023-02-27 18:53:18 +0000

    Tests

--- test/overlay_banner_test.dart
+++ test/overlay_banner_test.dart
@@ -1,12 +1,53 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:overlay_banner/overlay_banner.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('OverlayNotifier test', () {
+    test("showBanner saves the OverlayBanner", () {
+      var overlayBanner = OverlayBanner(body: Text("TESTING"));
+      var overlayNotifier = OverlayNotifier();
+      expect(overlayNotifier.banner, isNull);
+      overlayNotifier.showBanner(banner: overlayBanner);
+      expect(overlayNotifier.banner, isNotNull);
+    });
+
+    test("showBanner updates the OverlayBanner", () {
+      var overlayBanner = const OverlayBanner(
+        body: Text("TESTING"),
+        backgroundColour: Colors.red,
+      );
+      var overlayBanner2 = const OverlayBanner(
+        body: Text("TESTING"),
+        backgroundColour: Colors.green,
+      );
+      var overlayNotifier = OverlayNotifier();
+      overlayNotifier.showBanner(banner: overlayBanner);
+      expect(overlayNotifier.banner, isNotNull);
+      expect(overlayNotifier.banner!.backgroundColour, Colors.red);
+      overlayNotifier.showBanner(banner: overlayBanner2);
+      expect(overlayNotifier.banner!.backgroundColour, Colors.green);
+    });
+  });
+
+  test("showBanner hide the OverlayBanner", () {
+    var overlayBanner = const OverlayBanner(body: Text("TESTING"));
+    var overlayNotifier = OverlayNotifier();
+    overlayNotifier.showBanner(banner: overlayBanner);
+    expect(overlayNotifier.banner, isNotNull);
+    overlayNotifier.hideBanner();
+    expect(overlayNotifier.banner, isNull);
+  });
+
+  test("showBanner autoDismiss the OverlayBanner", () async {
+    var overlayBanner = const OverlayBanner(body: Text("TESTING"));
+    var overlayNotifier = OverlayNotifier();
+    overlayNotifier.showBanner(
+        banner: overlayBanner, autoDismissTime: const Duration(seconds: 2));
+    expect(overlayNotifier.banner, isNotNull);
+    await Future.delayed(
+      const Duration(seconds: 5),
+    );
+    expect(overlayNotifier.banner, isNull);
   });
 }
